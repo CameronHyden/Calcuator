054b43bd74fd5840fb82f72864dc96ef41fa57d2
CameronHyden <camh.hyden@gmail.com>
2022-08-16 08:21:49 +0100

    basic calculations added

---
+++ Calculator1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+namespace Calculator
+{
+    public class Calculator1
+    {
+
+
+        public static int add(int firstSum, int secondSum)
+        {
+            int result = firstSum + secondSum;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int subtract(int firstSum, int secondSum)
+        {
+            int result = firstSum - secondSum;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int divide(int firstSum, int secondSum)
+        {
+            int result = firstSum / secondSum;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int times(int firstSum, int secondSum)
+        {
+           int result = firstSum * secondSum;
+            Console.WriteLine(result);
+            return result;
+        }
+        public static void checkIfSquareRoot( string method, int firstSum)
+        {
+            if(method == "√")
+            {
+               
+                Calculator1.squareRoot(firstSum);
+            }
+        }
+        public static int squareRoot(int firstSum)
+        {
+            int result = (int)Math.Sqrt(firstSum);
+            Console.WriteLine(result);
+            Calculator1.exit(1);
+            return result;
+           
+        }
+            
+        public static void checkOperator(string method, int firstSum, int secondSum)
+        {
+            switch (method) {
+                case "+":
+                    Calculator1.add(firstSum, secondSum);
+                    break;
+                case "-":
+                    Calculator1.subtract(firstSum, secondSum);
+                    break;
+                case "/":
+                    Calculator1.divide(firstSum, secondSum);
+                    break;
+                case "*":
+                    Calculator1.times(firstSum, secondSum);
+                    break;
+                default:
+                    Console.WriteLine("sorry I dont accept that operator");
+                    break;
+            }
+
+        }
+        public static void exit(int position)
+        {
+
+            Environment.Exit(position);
+
+        }
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstSum = 0;
+            int secondSum = 0;
+            string method = "";
+
+
+            Console.WriteLine("Type a number, and then press Enter");
+            firstSum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type your operator, and then press Enter");
+            method = Console.ReadLine()!;
+            Calculator1.checkIfSquareRoot( method, firstSum);          
+            Console.WriteLine("Type another number, and then press Enter");
+            secondSum = Convert.ToInt32(Console.ReadLine());
+            Calculator1.checkOperator(method, firstSum, secondSum); 
+
+        }
+
+  
+    }
+}
